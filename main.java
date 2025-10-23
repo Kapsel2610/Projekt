50d6a5a5828dc4476af238ac48cb4f849976a55f
Kapsel2610 <kacper.raszewski26@gmail.com>
2025-10-23 12:24:40 +0200

    main.java

--- main.java
+++ main.java
@@ -4,18 +4,19 @@ public class Main {
         Book book1 = new Book("Wiedźmin", 59.99, "Andrzej Sapkowski");
         Clothing shirt = new Clothing("Koszulka", 49.99, "M");
 
-        // Produkty przed zniżką
+        
         System.out.println("=== Przed zniżką ===");
         book1.showInfo();
         shirt.showInfo();
 
-        // Zastosowanie zniżek
-        book1.applyDiscount(10);   // 10% zniżki
-        shirt.applyDiscount(20);   // 20% zniżki
+      
+        book1.applyDiscount(10);   
+        shirt.applyDiscount(20);  
 
-        // Produkty po zniżce
+    
         System.out.println("\n=== Po zniżce ===");
         book1.showInfo();
         shirt.showInfo();
     }
+
 }
